5daa700c91fb0757a4b479b62b885aadf65d347f
yurigarrido <y.garrido.yuri@gmail.com>
2023-05-30 21:34:04 -0300

    feat: add header and carousel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,23 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 
+import logoImage from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app'
+import Image from 'next/image'
+
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
 
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image
+          src={logoImage}
+          alt=''
+        />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,52 @@
 import { Inter } from 'next/font/google'
+import { HomeContainer, Product } from '../styles/pages/home'
+import Image from 'next/image'
+
+import camiseta1 from '../assets/camisetas/IgniteLab-T-shirt 1.png'
+import camiseta2 from '../assets/camisetas/IgniteLab-T-shirt 1.png'
+
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
   return (
-    <h1>
-     Hello World!
-    </h1>
+    <HomeContainer ref={sliderRef} className='keen-slider'>
+      <Product className='keen-slider__slide'>
+      <Image src={camiseta1} width={520} height={480} alt=''/>
+      <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 79,90</span>
+      </footer>
+     </Product>
+      <Product className='keen-slider__slide'>
+      <Image src={camiseta2} width={520} height={480} alt=''/>
+      <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 49,90</span>
+      </footer>
+     </Product>
+      <Product className='keen-slider__slide'>
+      <Image src={camiseta2} width={520} height={480} alt=''/>
+      <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 49,90</span>
+      </footer>
+     </Product>
+     <Product className='keen-slider__slide'>
+      <Image src={camiseta2} width={520} height={480} alt=''/>
+      <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 49,90</span>
+      </footer>
+     </Product>
+    </HomeContainer>
   )
 }
